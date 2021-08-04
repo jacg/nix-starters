7e9d17deae45428a10eb106011a6947bcbc3acde
Jacek Generowicz <jacg@my-post-office.net>
2021-08-04 22:24:18 +0200

    Add Python support

---
+++ python/shell.nix
@@ -0,0 +1,37 @@
+# TODO Hacking around the Qt problems
+# TODO PyPI package not in nixpkgs
+
+# To get a more recent version of nixpkgs, go to https://status.nixos.org/,
+# which lists the latest commit that passes all the tests for any release.
+# Unless there is an overriding reason, pick the latest stable NixOS release, at
+# the time of writing this is nixos-21.05.
+
+{
+  py ? "38" # To override the default python version:  nix-shell shell.nix --argstr py 37
+}:
+
+let
+  # ----- Pinned nixpkgs with -----------------------------------------------------------------------
+
+  nixpkgs-commit-id = "d4590d21006387dcb190c516724cb1e41c0f8fdf"; # nixos-21.05 on 2021-08-03
+  nixpkgs-url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-commit-id}.tar.gz";
+  pkgs = import (fetchTarball nixpkgs-url) {
+      overlays = map (uri: import (fetchTarball uri)) [];
+  };
+
+  # ----- Python version ----------------------------------------------------------------------------
+  python = builtins.getAttr ("python" + py) pkgs;
+  pypkgs = python.pkgs;
+
+  # ----- Choice of included packages ---------------------------------------------------------------
+
+  buildInputs = [
+    pypkgs.pytest
+  ];
+
+in
+
+pkgs.mkShell {
+  name = "my-rust-project";
+  buildInputs = buildInputs;
+}

6d505102032eb766833c2f3e5bf844c8350be4eb
Jacek Generowicz <jacg@my-post-office.net>
2021-08-05 20:15:05 +0200

    Add personal git configuration

--- home-manager/nixpkgs/home.nix
+++ home-manager/nixpkgs/home.nix
@@ -7,6 +7,10 @@ let
   pkgs = sources.pkgs;
   p10k-theme = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
   link = config.lib.file.mkOutOfStoreSymlink;
+
+  myName     = "Jacek Generowicz";
+  myGitEmail = "jacg@my-post-office.net";
+
 in
 
 {
@@ -48,7 +52,7 @@ in
     mosh        # more robust alternative to ssh
     sd          # intuitive find and replace CLI
 
-    git git-lfs github-cli
+    github-cli # git and git-lfs enabled below
 
     # Golden oldies
     file lsof
@@ -63,4 +67,55 @@ in
     settings.cpu_count_from_zero = true;
   };
 
+  programs.git = {
+
+    enable = true;
+    userName = myName;
+    userEmail = myGitEmail;
+
+    aliases = {
+      authors    = "!\"${pkgs.git}/bin/git log --pretty=format:%aN"
+                 + " | ${pkgs.coreutils}/bin/sort"
+                 + " | ${pkgs.coreutils}/bin/uniq -c"
+                 + " | ${pkgs.coreutils}/bin/sort -rn\"";
+      ls-ignored = "ls-files --exclude-standard --ignored --others";
+      undo       = "reset --soft HEAD^";
+      wdiff      = "diff --color-words";
+      l          = "log --graph --pretty=format:'%Cyellow%h%Creset"
+                 + " -%Cblue%d%Creset %s %Cgreen(%cr)%Creset'"
+                 + " --abbrev-commit --date=relative --show-notes=*";
+      lg         = "log --graph --decorate --oneline";
+      lga        = "log --graph --decorate --oneline --all";
+      man        = "ls-tree -r --name-only --full-tree HEAD";
+      manifest   = "man";
+      st         = "status";
+
+      # git change-commits GIT_COMMITTER_NAME "old name" "new name" [first..last]
+      change-commits = ''"!f() { VAR=$1; OLD=$2; NEW=$3; shift 3; git filter-branch --env-filter \"if [[ \\\"$`echo $VAR`\\\" = '$OLD' ]]; then export $VAR='$NEW'; fi\" $@; }; f "'';
+      # from https://help.github.com/articles/remove-sensitive-data
+      remove-file = "!f() { git filter-branch -f --index-filter \"git rm --cached --ignore-unmatch $1\" --prune-empty --tag-name-filter cat -- --all; }; f";
+
+    };
+
+    lfs.enable = true;
+
+    extraConfig = {
+      core = {
+        editor            = "${pkgs.emacs}/bin/emacsclient -c";
+        #whitespace        = "trailing-space,space-before-tab";
+        };
+
+      github.user           = "jacg";
+      user.name             = myName;
+      user.email            = myGitEmail;
+      #github.oauth    How do you keep this secret?
+      #credential.helper     = "${pkgs.pass-git-helper}/bin/pass-git-helper";
+      #ghi.token             = "!${pkgs.pass}/bin/pass api.github.com | head -1";
+      #hub.protocol          = "${pkgs.openssh}/bin/ssh";
+      mergetool.keepBackup  = true;
+      pull.rebase           = true;
+      #rebase.autosquash     = true;
+      rerere.enabled        = true;
+    };
+  };
 }

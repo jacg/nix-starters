ceb38aec3534495d530bbf4d58dd75ab6c8bd1d5
Jacek Generowicz <jacg@my-post-office.net>
2021-08-16 18:33:13 +0200

    Replace print with @testset

--- julia/testme.jl
+++ julia/testme.jl
@@ -1,4 +1,5 @@
 using Test
-@test 2 + 2 == 4
-@test π ≈ 3.15 atol = 0.01
-println("A couple of tests ran.")
+@testset "trivial stuff" begin
+    @test 2 + 2 == 4
+    @test π ≈ 3.15 atol = 0.01
+end

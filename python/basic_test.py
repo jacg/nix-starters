86bec587695be8c7f88403325d2bf7c50f5f8edb
Jacek Generowicz <jacg@my-post-office.net>
2021-11-04 13:15:23 +0100

    Test python version

--- python/basic_test.py
+++ python/basic_test.py
@@ -4,3 +4,10 @@ def test_one():
 def test_nonstandard_module():
     import numpy as np
     assert np.linspace(1,2).shape == (50,)
+
+def test_version():
+    import sys, os
+    vi = sys.version_info
+    found = f'{vi.major}{vi.minor}'
+    expected = os.getenv('EXPECTED_PY')
+    assert(found == expected)

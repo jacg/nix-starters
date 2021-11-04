1dccd3e7bfeec97053daef76bdeb66dc99581c41
Jacek Generowicz <jacg@my-post-office.net>
2021-11-04 13:30:11 +0100

    Skip python version test unless on GHA

--- python/basic_test.py
+++ python/basic_test.py
@@ -1,3 +1,6 @@
+import os
+from pytest import mark
+
 def test_one():
     assert 2 + 2 == 4
 
@@ -5,8 +8,10 @@ def test_nonstandard_module():
     import numpy as np
     assert np.linspace(1,2).shape == (50,)
 
+@mark.skipif(os.getenv('GITHUB_ACTIONS') != 'true',
+             reason = 'Not running on GHA')
 def test_version():
-    import sys, os
+    import sys
     vi = sys.version_info
     found = f'{vi.major}{vi.minor}'
     expected = os.getenv('EXPECTED_PY')

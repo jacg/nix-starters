a6d50008a146502254bc705f747bf274dd27497b
Jacek Generowicz <jacg@my-post-office.net>
2022-01-29 18:15:57 +0100

    Flakify julia

---
+++ julia/flake.nix
@@ -0,0 +1,31 @@
+# The core functionality is provided here using flakes. Legacy support for
+# `nix-shell` is provided by a wrapper in `shell.nix`.
+
+
+{
+  description = "Julia development environment";
+
+  inputs = {
+
+    nixpkgs     .url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils .url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              pkgs.julia-bin
+            ];
+          };
+        }
+    );
+}

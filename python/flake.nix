a9ba8b33ddffdf4c59bb6cd7d8bfa2627c0c4158
Jacek Generowicz <jacg@my-post-office.net>
2022-01-29 17:48:36 +0100

    Flakify python

---
+++ python/flake.nix
@@ -0,0 +1,41 @@
+# The core functionality is provided here using flakes. Legacy support for
+# `nix-shell` is provided by a wrapper in `shell.nix`.
+
+# TODO Hacking around the Qt problems
+# TODO PyPI package not in nixpkgs
+
+
+{
+  description = "Python development environment";
+
+  inputs = {
+
+    nixpkgs     .url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils .url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+          python = pkgs.python39;
+
+          # ----- A Python interpreter with the packages that interest us -------
+          python-with-all-my-packages = (python.withPackages (ps: [
+            ps.pytest
+            ps.numpy
+          ]));
+      in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              python-with-all-my-packages
+            ];
+          };
+        }
+    );
+}
--- python/shell.nix
+++ python/shell.nix
@@ -1,41 +1,15 @@
-# TODO Hacking around the Qt problems
-# TODO PyPI package not in nixpkgs
+# Adapts flake.nix to support legacy `nix-shell`
 
-# To get a more recent version of nixpkgs, go to https://status.nixos.org/,
-# which lists the latest commit that passes all the tests for any release.
-# Unless there is an overriding reason, pick the latest stable NixOS release, at
-# the time of writing this is nixos-21.05.
-
-{
-  py ? "39" # To override the default python version:  nix-shell shell.nix --argstr py 37
-}:
-
-let
-  # ----- Pinned nixpkgs ----------------------------------------------------------------------------
-
-  nixpkgs-commit-id = "604c44137d97b5111be1ca5c0d97f6e24fbc5c2c"; # nixos-21.11 on 2022-01-24
-  nixpkgs-url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-commit-id}.tar.gz";
-  pkgs = import (fetchTarball nixpkgs-url) {
-      overlays = map (uri: import (fetchTarball uri)) [];
-  };
-
-  # ----- Python version ----------------------------------------------------------------------------
-  python = builtins.getAttr ("python" + py) pkgs;
-
-  # ----- A Python interpreter with the packages that interest us -----------------------------------
-  python-with-all-my-packages = (python.withPackages (ps: [
-      ps.pytest
-      ps.numpy
-    ]));
-
-  # ----- Choice of included packages ---------------------------------------------------------------
-  buildInputs = [
-    python-with-all-my-packages
-  ];
-
-in
-
-pkgs.mkShell {
-  name = "my-python-project";
-  buildInputs = buildInputs;
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
+  }).shellNix

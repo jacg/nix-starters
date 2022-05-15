667f2e52b5a476ba158362b807c90f35acf62036
Jacek Generowicz <jacg@my-post-office.net>
2022-05-15 20:48:34 +0200

    Refactor Rust flake

--- rust/flake.nix
+++ rust/flake.nix
@@ -37,74 +37,43 @@
 
         let pkgs = import nixpkgs {
               inherit system;
-              # Any overlays you need can go here
-              overlays = [ (import rust-overlay) ];
-            };
-            # ----- Rust versions and extensions ----------------------------------
-            extras = {
-              extensions = [
 
-                # Uncomment the extensions you want to enable, in the list below.
-                # Some are enabled by default.
 
-                # A history of available extensions is published on
-                #
-                #    https://rust-lang.github.io/rustup-components-history
-                #
-                # but this appears to contradict reality. To get an error message
-                # which lists all available components, uncomment the following
-                # line
+              overlays = [
+                # ===== Specification of the rust toolchain to be used ====================
+                rust-overlay.overlay (final: prev:
+                  let
+                    # If you have a rust-toolchain file for rustup, choose `rustup =
+                    # rust-tcfile` further down to get the customized toolchain
+                    # derivation.
+                    rust-tcfile  = final.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+                    rust-latest  = final.rust-bin.stable .latest      ;
+                    rust-beta    = final.rust-bin.beta   .latest      ;
+                    rust-nightly = final.rust-bin.nightly."2022-01-25";
+                    rust-stable  = final.rust-bin.stable ."1.58.1"    ; # nix flake lock --update-input rust-overlay
+                    rust-analyzer-preview-on = date:
+                      final.rust-bin.nightly.${date}.default.override
+                        { extensions = [ "rust-analyzer-preview" ]; };
+                  in
+                    rec {
+                      # The version of the Rust system to be used in buildInputs. Choose between
+                      # tcfile/latest/beta/nightly/stable (see above) on the next line
+                      rustup = rust-stable;
 
-                #  "does-not-exist" # Uncomment this line to generate list of extensions
-
-                # Extensions which might be available. Uncomment those you want to
-                # enable. Some are enabled by default.
-
-                # "cargo"                    # enabled by default
-                # "clippy"                   # enabled by default
-                # "clippy-preview"           # seems to give the same version as clippy
-                # "llvm-tools-preview"
-                # "miri"
-                # "miri-preview"
-                # "reproducible-artifacts"
-                # "rls"
-                # "rls-preview"              # seems to give the same version as rls
-                # "rust"
-                # "rust-analysis"
-                # "rust-analyzer-preview"
-                # "rust-docs"
-                # "rust-mingw"
-                # "rust-src"
-                # "rust-std"
-                # "rustc"
-                # "rustc-dev"
-                # "rustc-docs"
-                # "rustfmt"
-                # "rustfmt-preview"
+                      rustc = rustup.default;
+                      cargo = rustup.default;
+                      rust-analyzer-preview = rust-analyzer-preview-on "2022-01-25";
+                    })
               ];
-              #targets = [ "arg-unknown-linux-gnueabihf" ];
             };
-
-            # If you already have a rust-toolchain file for rustup, you can simply
-            # use fromRustupToolchainFile to get the customized toolchain
-            # derivation.
-            rust-tcfile  = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
-
-            rust-latest  = pkgs.rust-bin.stable .latest      .default;
-            rust-beta    = pkgs.rust-bin.beta   ."2022-01-25".default;
-            rust-nightly = pkgs.rust-bin.nightly."2022-01-25".default;
-            rust-stable  = pkgs.rust-bin.stable ."1.58.1"    .default;
-
-            # Rust system to be used in buldiInputs. Choose between
-            # latest/beta/nightly/stable on the next line
-            rust = rust-stable.override extras;
 
         in
           {
             devShell = pkgs.mkShell {
               name = "my-rust-project";
               buildInputs = [
-                rust
+                pkgs.rustc
+                pkgs.cargo
                 pkgs.rust-analyzer
                 pkgs.just
                 pkgs.cowsay

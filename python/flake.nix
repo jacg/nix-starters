09baf62a203affc45f7171d0ecf08824fa05367d
Jacek Generowicz <jacg@my-post-office.net>
2022-06-15 15:10:24 +0200

    Bump NixOS 21.11 -> 22.05

--- python/flake.nix
+++ python/flake.nix
@@ -15,7 +15,7 @@
     #
     #    nix flake lock --update-input nixpkgs
 
-    nixpkgs     .url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs     .url = "github:nixos/nixpkgs/nixos-22.05";
     flake-utils .url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
--- rust/flake.nix
+++ rust/flake.nix
@@ -16,7 +16,7 @@
     #
     #    nix flake lock --update-input nixpkgs
 
-    nixpkgs     .url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs     .url = "github:nixos/nixpkgs/nixos-22.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils .url = "github:numtide/flake-utils";
     flake-compat = {

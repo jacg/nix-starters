a6cd115a4e4d3d48e074e058ecf62203f5f4ea52
Jacek Generowicz <jacg@my-post-office.net>
2022-11-28 07:06:41 +0100

    Bump rust 1.64 -> 1.65

--- rust/flake.nix
+++ rust/flake.nix
@@ -49,8 +49,8 @@
                     rust-tcfile  = final.rust-bin.fromRustupToolchainFile ./rust-toolchain;
                     rust-latest  = final.rust-bin.stable .latest      ;
                     rust-beta    = final.rust-bin.beta   .latest      ;
-                    rust-nightly = final.rust-bin.nightly."2022-10-16";
-                    rust-stable  = final.rust-bin.stable ."1.64.0"    ; # nix flake lock --update-input rust-overlay
+                    rust-nightly = final.rust-bin.nightly."2022-11-04";
+                    rust-stable  = final.rust-bin.stable ."1.65.0"    ; # nix flake lock --update-input rust-overlay
                     rust-analyzer-preview-on = date:
                       final.rust-bin.nightly.${date}.default.override {
                         extensions = [ "rust-analyzer-preview" ];
@@ -67,7 +67,7 @@
                         # extensions = [];
                         # targets = [ "wasm32-unknown-unknown" ];
                       };
-                      rust-analyzer-preview = rust-analyzer-preview-on "2022-10-16";
+                      rust-analyzer-preview = rust-analyzer-preview-on "2022-11-04";
                       rust-src = rust-stable.rust-src;
                     })
               ];

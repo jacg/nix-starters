b8fbefb0bd4cf86a2ea49ca1c63d5bd058f6c64b
Jacek Generowicz <jacg@my-post-office.net>
2024-09-29 10:13:30 +0200

    Bump NixOS version in typst

--- typst/flake.nix
+++ typst/flake.nix
@@ -2,7 +2,7 @@
   description = "Typst user environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs, ... }:

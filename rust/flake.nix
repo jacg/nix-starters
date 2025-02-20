5f1dd2d4902ae8cbc9a75761f9b4b5f16f90715c
Jacek Generowicz <jacg@my-post-office.net>
2025-02-20 08:52:07 +0100

    Reword Rust flake inputs

--- rust/flake.nix
+++ rust/flake.nix
@@ -6,20 +6,18 @@
   description = "Rust development environment";
 
   inputs = {
-
-    # Version pinning is managed in flake.lock. Upgrading can be done with
-    # something like
+    # Version pinning is managed in flake.lock.
+    # Upgrading can be done with `nix flake lock --update input <input-name>`
     #
     #    nix flake lock --update-input nixpkgs
-
-    nixpkgs     .url = "github:nixos/nixpkgs/nixos-24.11";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils .url = "github:numtide/flake-utils";
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-24.11"; # nix flake lock --update input nixpkgs
+    rust-overlay.url = "github:oxalica/rust-overlay";      # nix flake lock --update input rust-overlay
+    flake-utils.url  = "github:numtide/flake-utils";
+    # Support for legacy nix-shell
     flake-compat = {
-      url = "github:edolstra/flake-compat";
+      url   = "github:edolstra/flake-compat";
       flake = false;
     };
-
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:

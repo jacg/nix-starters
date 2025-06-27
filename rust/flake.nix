e9fb0fa17c2daeba91b466e26176d987ac5da421
Jacek Generowicz <jacg@my-post-office.net>
2025-06-27 10:01:02 +0200

    Alignment

--- rust/flake.nix
+++ rust/flake.nix
@@ -10,9 +10,9 @@
     # Upgrading can be done with `nix flake lock --update input <input-name>`
     #
     #    nix flake lock --update-input nixpkgs
-    nixpkgs.url      = "github:nixos/nixpkgs/nixos-25.05"; # nix flake lock --update input nixpkgs
+    nixpkgs     .url = "github:nixos/nixpkgs/nixos-25.05"; # nix flake lock --update input nixpkgs
     rust-overlay.url = "github:oxalica/rust-overlay";      # nix flake lock --update input rust-overlay
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils .url = "github:numtide/flake-utils";
     # Support for legacy nix-shell
     flake-compat = {
       url   = "github:edolstra/flake-compat";

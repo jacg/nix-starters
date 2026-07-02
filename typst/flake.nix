cdd550c82b29fffe63d4018e74d85dbbe638c64f
Jacek Generowicz <jacg@my-post-office.net>
2026-07-02 11:31:20 +0200

    Typst: bump nix 25.05 -> 26.05

--- typst/flake.nix
+++ typst/flake.nix
@@ -2,7 +2,7 @@
   description = "Typst user environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs, ... }:
